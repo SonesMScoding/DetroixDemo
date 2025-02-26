b3c2e103bb7d60b62201ea50989b7ce1d240fe5a
SonesMScoding <eve.sones@mscoding.org>
2025-02-26 09:22:47 -0600

    front-end css, bottom half of the page

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -8,7 +8,7 @@ function currentSlide(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-let dots = document.getElementsByClassName("dot");
+  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -16,7 +16,7 @@ let dots = document.getElementsByClassName("dot");
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
-}
+  }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
